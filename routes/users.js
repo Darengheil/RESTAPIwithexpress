32cec65e4eb6ba213516452967631c1530362612
Darengheil <einhvas@gmail.com>
2021-11-15 08:49:58 +0300

    added users to test

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstname: "John",
+        lastname: "Doe",
+        age: 25
+    },
+    {
+        firstname: "Jane",
+        lastname: "Willson",
+        age: 24
+    }
+];
+
 router.get('/', (req, res) => {
-    res.send('Hello it is users page');
+    console.log(users);
+
+    res.send(users);
 });
+
+
 
 export default router;
